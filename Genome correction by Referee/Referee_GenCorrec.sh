f582f04f0458bdadde6cd5a0c85eccb2eb1d191a
Pankaj Dhakad <63622417+pd16@users.noreply.github.com>
2021-02-25 15:45:31 +0530

    Create Referee_GenCorrec.sh

---
+++ Genome correction by Referee/Referee_GenCorrec.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#This script uses the BWA-MEM aligner to map the raw reads on to the genome and Referee to correct the genome.
+
+for i in $(ls -lrt|cut -d " " -f9|grep ".fastq.gz"|awk '{gsub("_1.fastq.gz","",$0);gsub("_2.fastq.gz","",$0);print $0}'|sort -u)
+
+do
+
+gunzip "$i"_1.fastq.gz
+gunzip "$i"_2.fastq.gz
+
+~/bwa-0.7.17/bwa mem -t 16 ~/viplav/DNM1/Chlorocebus_sabaeus_genome/GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.fna "$i"_1.fastq "$i"_2.fastq > reads-mapped-"$i".sam
+
+#Picard will modify the SAM header
+java -jar ~/picard/build/libs/picard.jar ReplaceSamHeader I=reads-mapped-"$i".sam HEADER=reference.dict O=reads-mapped-"$i"-hd.sam
+
+rm reads-mapped-"$i".sam
+
+~/samtools-1.10/samtools sort -@16 reads-mapped-"$i"-hd.sam -o "$i"_aligned.bam
+
+rm reads-mapped-"$i"-hd.sam
+
+~/samtools-1.10/samtools index -b -@16 "$i"_aligned.bam
+
+~/referee-1.1.21/helper-scripts/./bam_split.sh -b "$i"_aligned.bam -f ~/viplav/DNM1/Chlorocebus_sabaeus_genome/GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.fna -a
+
+wait
+
+cd ~/IKBIP_gene_selection_project/DNM1_SRA/Chloro_sab_WGS_SRA/khorana2/bamsplit-angsd
+
+for i in `ls -1 *glf.gz`
+do
+
+python ~/referee-1.1.21/referee.py -gl *.glf.gz -ref ~/viplav/DNM1/Chlorocebus_sabaeus_genome/GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.fna -p 16 --correct -o "${i%.glf.gz}"-referee-corrected
+
+done
+done
