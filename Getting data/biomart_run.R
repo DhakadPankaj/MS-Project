7cb519e6e6a9e478c77c6c046b332a60daa28b0f
Pankaj Dhakad <63622417+pd16@users.noreply.github.com>
2021-02-25 16:26:09 +0530

    Update biomart_run.R

--- Getting data/biomart_run.R
+++ Getting data/biomart_run.R
@@ -6,8 +6,3 @@ mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 seq <- getSequence(id=TID[1],seqType="coding", type="ensembl_transcript_id",mart = mart)
 
 exportFASTA(seq,file=paste(TID[2],"/ncbi_dataset/data/",TID[1],"ref.fa", sep = ""))
-
-
-
-
-
