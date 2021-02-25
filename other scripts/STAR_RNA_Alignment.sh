093e922465a943345f4523f6287470237ac42e61
Pankaj Dhakad <63622417+pd16@users.noreply.github.com>
2021-02-25 16:10:17 +0530

    Update and rename STAR_aligner.txt to other scripts/STAR_RNA_Alignment.sh

--- other scripts/STAR_RNA_Alignment.sh
+++ other scripts/STAR_RNA_Alignment.sh
@@ -15,10 +15,7 @@ samtools index -b -@16 "$i"_rna-Aligned-sorted.out.bam
 
 done
 
+#nohup ~/STAR-2.7.3a/source/./STAR --runMode alignReads --outSAMtype BAM SortedByCoordinate --genomeDir /workdirectory/STAR-RNAseq/STAR_ref_genome --outFileNamePrefix rna_map  --readFilesIn  *_1.fastq *_2.fastq --runThreadN 5 &
 
-
-nohup ~/STAR-2.7.3a/source/./STAR --runMode alignReads --outSAMtype BAM SortedByCoordinate --genomeDir /workdirectory/STAR-RNAseq/STAR_ref_genome --outFileNamePrefix rna_map  --readFilesIn  *_1.fastq *_2.fastq --runThreadN 5 &
-
-
-nohup ~/STAR-2.7.3a/source/./STAR --runThreadN 8 --runMode genomeGenerate --genomeDir ~/viplav/DNM1/Chlorocebus_sabaeus_genome/STAR-rna-seq --genomeFastaFiles GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.fna  --sjdbGTFfile GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.gtf &
+#nohup ~/STAR-2.7.3a/source/./STAR --runThreadN 8 --runMode genomeGenerate --genomeDir ~/viplav/DNM1/Chlorocebus_sabaeus_genome/STAR-rna-seq --genomeFastaFiles GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.fna  --sjdbGTFfile GCF_000409795.2_Chlorocebus_sabeus_1.1_genomic.gtf &
 
