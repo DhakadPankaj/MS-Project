4ad85ff58ae36eb06da9e0375f456054a48fce1e
Pankaj Dhakad <63622417+pd16@users.noreply.github.com>
2021-02-25 16:49:44 +0530

    Delete fasta_head_modif.sh

--- fasta_head_modif.sh
+++
@@ -1,2 +0,0 @@
-awk 'BEGIN{FS="PREDICTED: "}{if($1~">"){gsub(">","");print ">"$2"_"$3} else {print $0}}' file|awk '{if($1~">") {gsub("IKBKB","iso1", $3); print $1" "$2" "$3} else {print $0}}'|awk '{if($1~">") {gsub(" ","_", $0); print $1} else {print $0}}'  > temp.fa
-
